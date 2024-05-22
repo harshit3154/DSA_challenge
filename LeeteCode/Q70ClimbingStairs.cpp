593242166557c75c09660ca4285a11936ba63f8e
harshit kumar <harshitkr9696@gmail.com>
2024-05-22 20:12:09 +0530

    AI

--- LeeteCode/Q70ClimbingStairs.cpp
+++ LeeteCode/Q70ClimbingStairs.cpp
@@ -13,10 +13,19 @@ public:
        }
        return b;
     }
+    
+    int climbStairsd(int n) {
+		int (*fp)(int);
+		fp=climbStairsd;
+       if(n<4)  return n;
+       return fp(n-1)+fp(n-2);
+   }
 };
 
 int main(){
     Solution *ob=new Solution();
-    ob->climbStairs(5);
+    for(int i=1;i<14;i++){
+    	cout<<ob->climbStairs(i)<<"\t"<<ob->climbStairsd(i)<<endl;
+	}
     return 0;
 }
--- udemy/AI/pand.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data=pd.read_csv('harshit.csv')
-pd.Series(data)
-print(pd.Series(data[0]))
